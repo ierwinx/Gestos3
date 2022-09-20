74e088c9adb6e8061e0126e1e8ea834740eab0b0
Eяw!и Gontier <ierwinx@me.com>
2022-09-20 15:45:13 -0500

    Initial Commit

---
+++ Gestos3/Gestos3App.swift
@@ -0,0 +1,17 @@
+//
+//  Gestos3App.swift
+//  Gestos3
+//
+//  Created by Erwin Luz León on 20/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Gestos3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
